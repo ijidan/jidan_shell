970bae66d7ceda1d40eafd96d612536328934789
fp-jidan <394777474@qq.com>
2016-08-22 14:16:49 +0800

    memcached

--- install_php_memcached_extension.sh
+++ install_php_memcached_extension.sh
@@ -1,10 +1,24 @@
 #/bin/bash
 
-cd /usr/local/src
-wget http://pecl.php.net/get/memcached-2.2.0.tgz
-tar -zxvf memcached-2.2.0.tgz
-cd memcached-2.2.0
-/usr/local/php/bin/phpize
-./configure --with-php-config=/usr/local/php/bin/php-config --enable-memcached --with-libmemcached-dir=/usr/local/libmemcached-1.0.18 --disable-memcached-sasl
-make 
-make install
+SRC_PATH=/usr/local/src
+FILE_NAME=memcached-2.2.0.tgz
+DIR_NAME=memcached-2.2.0
+PHP_DIR=/usr/local/php
+MEMCACHED_DIR=/usr/local/libmemcached-1.0.18
+
+if [ grep extension=\"memcached.so\"  $PHP_DIR/etc/php.ini ];then
+	echo "memcached extension has been installed!"
+	exit
+fi
+cd $SRC_PATH
+if [ !-f $FILE_NAME];then
+	wget http://pecl.php.net/get/$FILE_NAME
+fi
+if [!-d $DIR_NAME];then
+	tar -zxvf $FILE_NAME
+fi
+cd $DIR_NAME
+$PHP_DIR/bin/phpize
+./configure --with-php-config=$PHP_DIR/bin/php-config --enable-memcached --with-libmemcached-dir=$MEMCACHED_DIR --disable-memcached-sasl
+make && make install
+ 
