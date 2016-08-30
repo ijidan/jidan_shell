ab4d9e22f34054eba01826701aa09061c2911914
fp-jidan <394777474@qq.com>
2016-08-30 17:08:20 +0800

    add php.sh

--- common.sh
+++ common.sh
@@ -6,45 +6,60 @@ export PATH
 readonly LOCAL_DIR=/usr/local
 readonly SRC_DIR=${LOCAL_DIR}/src
 readonly LOCAL_BIN_DIR=${LOCAL_DIR}/bin
+readonly CUR_DIR=`pwd`
+
+readonly PHP_VERSION="php-5.6.25"
+
 
 function isProgramInstalled(){
-	_installed=`command -v $1 >/dev/null 2>&1`
-	if [[ ${_installed} == ""  ]]
-	then
-		return 0
-	else
-		return 1
-	fi
+        _installed=`command -v $1 >/dev/null 2>&1`
+        if [[ ${_installed} == ""  ]]  
+        then
+                return 0
+        else
+                return 1
+        fi  
 }
 
 function isProgramRunning(){
-	 _ps=`pgrep $MONGOD_CMD |wc -l`
-	 if [ ${_ps} -eq 0 ]
-	 then 
-		return 1
-	 else
-		return 0
-	 fi 
+         _ps=`pgrep $MONGOD_CMD |wc -l`
+         if [ ${_ps} -eq 0 ] 
+         then 
+                return 1
+         else
+                return 0
+         fi  
 }
 
 function is64Bit(){
-	uname -a | grep "x86_64" 
-	if [[ "$?" -eq "0" ]]
-	then
-		return 1
-	else
-		return 0
-	fi
+        uname -a | grep "x86_64" 
+        if [[ "$?" -eq "0" ]]
+        then
+                return 1
+        else
+                return 0
+        fi  
 }
 function makeDir(){
-	if [ ! -d $1 ]
-	then
-		mkdir -r $1
-	fi
+        if [ ! -d $1 ]
+        then
+                mkdir -r $1
+        fi  
 }
 function makeFile(){
-	if [ ! -f $1 ]
-	then
-		touch $1
-	fi
+        if [ ! -f $1 ]
+        then
+                touch $1
+        fi  
 }
+function tarAndCd(){
+	local file_name=$1
+	local dir_name=$2
+	cd ${CUR_DIR}/src
+	[[ -d ${dir_name} ]] && rm -rf ${dir_name}
+	echo "unpress ${file_name}"
+	tar -zxvf ${$file_name}
+	echo "cd ${dir_name}"
+	cd ${dir_name} 	
+}
+
--- test.sh
+++ test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-_64bit="Linux localhost.localdomain 2.6.32-504.el6.x86_64 #1 SMP Wed Oct 15 04:27:16 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux"
-_32bit="Linux vagrant-centos62 2.6.32-573.26.1.el6.i686 #1 SMP Tue May 3 21:55:43 UTC 2016 i686 i686 i386 GNU/Linux"
 
-echo "${_64bit}" | grep "x86_64"
+function testReturn(){
+	if test true
+	then
+		return 0
+	else
+		return 0
 
-echo $?
-
-echo "${_64bit}" | grep "i386"
-
-echo $?
+	fi
+}
+testReturn
+if [ testReturn ]
+then
+	echo "100"
+else
+	echo "0"
+fi
